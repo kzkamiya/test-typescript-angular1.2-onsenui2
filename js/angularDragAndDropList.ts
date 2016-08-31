620f260ffaeff5fc5a5c1e4e8876b48b0cb929ad
CMTEST080 <cmtest080@gmail.com>
2016-08-31 15:28:39 +0900

    Added a new ts.

---
+++ js/angularDragAndDropList.ts
@@ -0,0 +1,33 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+app.controller('AngularDragAndDropListController', ['$scope', 
+    
+    function($scope) {
+
+    $scope.models = {
+        selected: null,
+        lists: {"A": [], "B": []}
+    };
+
+    // Generate initial model
+    for (var i = 1; i <= 3; ++i) {
+        $scope.models.lists.A.push({label: "Item A" + i});
+        $scope.models.lists.B.push({label: "Item B" + i});
+    }
+
+    // Model to JSON for demo purpose
+    $scope.$watch('models', function(model) {
+        $scope.modelAsJson = angular.toJson(model, true);
+    }, true);
+
+
+    $scope.openOrClose = function() {
+      if ($scope.mySplitterSide.mode == 'split') {
+          $scope.mySplitterSide.close();
+      } else {
+          $scope.mySplitterSide.open();
+      }
+    }
+}]);
+
+
